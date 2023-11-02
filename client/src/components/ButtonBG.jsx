1c61ff3bbdb1e7f389581e38e6f13ced8f0e690e
suhyunlee <leesuhyun05505@gmail.com>
2023-11-02 16:51:22 +0900

    Edit: 버튼 컴포넌트 수정
    버튼 컴포넌트 props로 컬러 변경하게 구현
    
    - backgroundColor, fontColor, hoverColor, children props가 필요합니다.
    
    - 사용 예시:
    <ButtonBg backgroundColor={COLOR.YELLOW} hoverColor = {COLOR.POTZ_PINK_DEFAULT} fontColor={COLOR.WHITE}>children</ButtonBg>

--- client/src/components/ButtonBG.jsx
+++ client/src/components/ButtonBG.jsx
@@ -1,28 +1,38 @@
 import styled from "styled-components"
-import COLOR from "../utility/Color"
 import Font from "../utility/Font"
 
-const ButtonBg = styled.button `
-    font-family: ${Font.FontKor};
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 500;
-    border : none;
-    border-radius: 12px;
-    display: flex;
-    width: 364px;
-    height: 46.667px;
-    padding: 9.333px 18.667px;
-    justify-content: center;
-    align-items: center;
-    gap: 11.667px;
-    background-color : ${COLOR.POTZ_PINK_DEFAULT};
-    color : ${COLOR.WHITE};
+//backgroundColor, fontColor, hoverColor, children props가 필요합니다.
+//사용 예시:
+//<ButtonBg backgroundColor={COLOR.YELLOW} hoverColor = {COLOR.POTZ_PINK_DEFAULT} fontColor={COLOR.WHITE}>children</ButtonBg>
 
-    // 호버 상태 스타일
-    &:hover {
-        background-color: ${COLOR.POTZ_PINK_600};${COLOR.POTZ_PINK_600};
-    }
-`
+const ButtonBg = (props) => {
+    let {backgroundColor, fontColor, hoverColor, children} = props;
+
+    const ButtonBgStyle = styled.button `
+        font-family: ${Font.FontKor};
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        border : none;
+        border-radius: 12px;
+        display: flex;
+        width: 364px;
+        height: 46.667px;
+        padding: 9.333px 18.667px;
+        justify-content: center;
+        align-items: center;
+        gap: 11.667px;
+        background-color : ${backgroundColor};
+        color : ${fontColor};
+
+        // 호버 상태 스타일
+        &:hover {
+            background-color: ${hoverColor};
+        }
+    `
+    return(
+        <ButtonBgStyle>{children}</ButtonBgStyle>
+    )
+}
 
 export default ButtonBg;
--- client/src/components/ButtonSM.jsx
+++ client/src/components/ButtonSM.jsx
@@ -1,27 +1,39 @@
 import styled from "styled-components";
-import COLOR from "../utility/Color";
 import Font from "../utility/Font";
 
-const ButtonSm = styled.button `
-    font-family: ${Font.FontKor};
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 500;
-    border : none;
-    border-radius: 12px;
-    display: flex;
-    width: 299px;
-    height: 47px;
-    padding: 9.333px 18.667px;
-    justify-content: center;
-    align-items: center;
-    gap: 11.667px;
-    background-color : ${COLOR.POTZ_PINK_DEFAULT};
-    color : ${COLOR.WHITE};
 
-    // 호버 상태 스타일
-    &:hover {
-        background-color: ${COLOR.POTZ_PINK_600};
-    }
-`
+//backgroundColor, fontColor, hoverColor, children props가 필요합니다.
+//사용 예시:
+//<ButtonSm backgroundColor={COLOR.YELLOW} hoverColor = {COLOR.POTZ_PINK_DEFAULT} fontColor={COLOR.WHITE}>children</ButtonSm>
+
+
+const ButtonSm = (props) => {
+    let {backgroundColor, fontColor, hoverColor, children} = props;
+
+    const ButtonStyle = styled.button `
+        font-family: ${Font.FontKor};
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        border : none;
+        border-radius: 12px;
+        display: flex;
+        width: 299px;
+        height: 47px;
+        padding: 9.333px 18.667px;
+        justify-content: center;
+        align-items: center;
+        gap: 11.667px;
+        background-color : ${backgroundColor};
+        color : ${fontColor};
+
+        // 호버 상태 스타일
+        &:hover {
+            background-color: ${hoverColor};
+        }
+    `
+    return(
+        <ButtonStyle>{children}</ButtonStyle>
+    )
+}
 export default ButtonSm;
