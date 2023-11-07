1a9745f29302dfaef5c545213bd368df4238cb10
uiyoung <suy2suy@gmail.com>
2023-11-07 17:41:14 +0900

    Refactor: post->posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,12 +5,20 @@ import {
   updatePost,
   deletePost,
   createPost,
-} from '../controllers/post.js';
+  toggleLike,
+  getSamplePosts,
+  createSamplePost,
+} from '../controllers/posts.js';
 
 const router = Router();
+
+// TODO: remove samples
+router.get('/sample', getSamplePosts);
+router.post('/sample', createSamplePost);
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.route('/:id').get(getPostById).patch(updatePost).delete(deletePost);
+router.patch('/:id/like', toggleLike);
 
 export default router;
