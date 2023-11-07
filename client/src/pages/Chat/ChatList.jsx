f2f87405fd1bef7e4a5335d7b66430a0d468a617
GISUNG KIM <nightfury805@gmail.com>
2023-11-07 19:23:45 +0900

    Refactor: 채팅페이지 리펙토링

--- client/src/pages/Chat/ChatList.jsx
+++ client/src/pages/Chat/ChatList.jsx
@@ -6,6 +6,8 @@ import styled from 'styled-components';
 import COLOR from '../../utility/Color';
 import Font from '../../utility/Font';
 import { NavBar3 } from '../../components/NavBars';
+import ChatSVg from '../Chat/ChatSvg';
+import TagFood from '../../components/TagFood';
 
 //contents_container 안에 UI 구현 하시면 됩니다!
 
@@ -57,20 +59,74 @@ function ChatList() {
     height: 30.33px;
     display: flex;
     flex-direction: row;
+    align-items: center;
     gap: 9.33px;
   `;
+  const GreenDot = styled.div`
+    width: 4.67px;
+    height: 4.67px;
+    background-color: ${COLOR.GREEN};
+    border-radius: 50%;
+  `;
+  const Alarm = styled.div`
+    width: 30.33px;
+    height: 18.67px;
+    background-color: ${COLOR.POTZ_PINK_DEFAULT};
+    border-radius: 11.6667px;
+    font-family: 'Noto Sans CJK KR';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 11.6667px;
+    color: ${COLOR.WHITE};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  const AlarmMessage = styled.div`
+    width: 75.83px;
+    height: 30.33px;
+    background: ${COLOR.POTZ_PINK_200};
+    border-radius: 44.3333px;
+    color: ${COLOR.POTZ_PINK_DEFAULT};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: ${Font.FontKor};
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+  `;
 
-  const FontBg = styled.p`
+  const FontBig = styled.p`
     font-family: ${Font.FontKor};
     font-style: normal;
     font-weight: 500;
     font-size: 16.3333px;
     color: ${COLOR.BLACK};
+    margin: 0;
+  `;
+  const FontMd = styled.p`
+    font-family: ${Font.FontKor};
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    color: ${(props) => props.color}; //${COLOR.BLACK}; ${COLOR.GRAY_400};
   `;
 
   const styles = {
     background: {
       backgroundColor: `${COLOR.GRAY_100}`,
+    },
+    sideTitle: {
+      marginTop: '70px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingLeft: '28px',
+      paddingRight: '28px',
+      backgroundColor: 'white',
+      height: '30.33px',
     },
     content: {
       display: 'flex',
@@ -90,6 +146,13 @@ function ChatList() {
       flexDirection: 'row',
       justifyContent: 'space-between',
     },
+    rowFlex: {
+      display: 'flex',
+      flexDirection: 'row',
+      gap: '7px',
+      alignItems: 'center',
+      height: '21px',
+    }
   };
 
   return (
@@ -101,20 +164,10 @@ function ChatList() {
         <Col className='col2'>
           <div className='potz_container' style={styles.background}>
             <Title>배달팟 채팅 목록</Title>
-            <div
-              style={{
-                marginTop: '70px',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                paddingLeft: '28px',
-                paddingRight: '28px',
-                backgroundColor: 'white',
-                height: '30.33px',
-              }}
-            >
-              <div>지금 참여중인 배달팟 채팅 4</div>
+            <div style={styles.sideTitle}>
+              <FontMd color={`${COLOR.GRAY_400}`}>
+                지금 참여중인 배달팟 채팅 4
+              </FontMd>
               <svg
                 width='7'
                 height='6'
@@ -138,17 +191,36 @@ function ChatList() {
                   />
                   <div>
                     <div style={styles.space}>
-                      <div>누들박스</div>
-                      <div>12</div>
+                      <FontBig>누들박스</FontBig>
+                      <Alarm>12</Alarm>
                     </div>
-                    <div>10명 참여중 | 1분 전</div>
+                    <div style={styles.rowFlex}>
+                    <GreenDot />
+                    <FontMd color={`${COLOR.GRAY_400}`}>
+                      10명 참여중 | 1분 전
+                    </FontMd>
+                    </div>
+
                   </div>
                 </Content1>
                 <Content2>
-                <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M22.1581 10.2621C22.1581 11.7273 22.5454 12.591 23.3976 13.5862C24.0434 14.3194 24.2498 15.2606 24.2498 16.2817C24.2498 17.3016 23.9147 18.2698 23.2434 19.0559C22.3645 19.9983 21.125 20.5999 19.86 20.7045C18.0268 20.8608 16.1924 20.9924 14.3338 20.9924C12.4739 20.9924 10.6407 20.9136 8.80755 20.7045C7.54137 20.5999 6.30186 19.9983 5.42411 19.0559C4.75276 18.2698 4.4165 17.3016 4.4165 16.2817C4.4165 15.2606 4.62406 14.3194 5.26874 13.5862C6.14764 12.591 6.50941 11.7273 6.50941 10.2621V9.76503C6.50941 7.80274 6.99872 6.51961 8.00633 5.26351C9.50441 3.43165 11.9057 2.33301 14.2816 2.33301H14.3859C16.8128 2.33301 19.2918 3.48453 20.7644 5.39512C21.7198 6.62537 22.1581 7.85444 22.1581 9.76503V10.2621ZM10.9192 23.4038C10.9192 22.8163 11.4584 22.5472 11.9569 22.4321C12.5402 22.3087 16.0941 22.3087 16.6773 22.4321C17.1759 22.5472 17.715 22.8163 17.715 23.4038C17.6861 23.9631 17.3579 24.459 16.9046 24.7739C16.3167 25.2321 15.6268 25.5224 14.9056 25.6269C14.5067 25.6786 14.1148 25.6798 13.7298 25.6269C13.0075 25.5224 12.3176 25.2321 11.7308 24.7727C11.2763 24.459 10.9482 23.9631 10.9192 23.4038Z" fill="#A8A8A8"/>
-</svg>
-<div>방장 요청사항</div> <div>2개</div>
+                  <svg
+                    width='29'
+                    height='28'
+                    viewBox='0 0 29 28'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d='M22.1581 10.2621C22.1581 11.7273 22.5454 12.591 23.3976 13.5862C24.0434 14.3194 24.2498 15.2606 24.2498 16.2817C24.2498 17.3016 23.9147 18.2698 23.2434 19.0559C22.3645 19.9983 21.125 20.5999 19.86 20.7045C18.0268 20.8608 16.1924 20.9924 14.3338 20.9924C12.4739 20.9924 10.6407 20.9136 8.80755 20.7045C7.54137 20.5999 6.30186 19.9983 5.42411 19.0559C4.75276 18.2698 4.4165 17.3016 4.4165 16.2817C4.4165 15.2606 4.62406 14.3194 5.26874 13.5862C6.14764 12.591 6.50941 11.7273 6.50941 10.2621V9.76503C6.50941 7.80274 6.99872 6.51961 8.00633 5.26351C9.50441 3.43165 11.9057 2.33301 14.2816 2.33301H14.3859C16.8128 2.33301 19.2918 3.48453 20.7644 5.39512C21.7198 6.62537 22.1581 7.85444 22.1581 9.76503V10.2621ZM10.9192 23.4038C10.9192 22.8163 11.4584 22.5472 11.9569 22.4321C12.5402 22.3087 16.0941 22.3087 16.6773 22.4321C17.1759 22.5472 17.715 22.8163 17.715 23.4038C17.6861 23.9631 17.3579 24.459 16.9046 24.7739C16.3167 25.2321 15.6268 25.5224 14.9056 25.6269C14.5067 25.6786 14.1148 25.6798 13.7298 25.6269C13.0075 25.5224 12.3176 25.2321 11.7308 24.7727C11.2763 24.459 10.9482 23.9631 10.9192 23.4038Z'
+                      fill='#A8A8A8'
+                    />
+                  </svg>
+                  <FontMd color={`${COLOR.BLACK}`}>방장 요청사항</FontMd>
+                  <FontMd color={`${COLOR.BLACK}`}>2개</FontMd>
+                  <AlarmMessage>정산해요</AlarmMessage>
                 </Content2>
               </Chat>
               <Chat></Chat>
