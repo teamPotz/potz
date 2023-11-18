a7f8a06aa9f3a2cb65f13f5a64de75e14988b90c
uiyoung <suy2suy@gmail.com>
2023-11-18 21:22:26 +0900

    Chore: session-file-store 추가, socket.io 추가

--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import FileStore from 'session-file-store';
 import passport from 'passport';
 import passportConfig from './passport/index.js';
 
+import webSocket from './socket.js';
 import authRouter from './routes/auth.js';
 import communitiesRouter from './routes/communities.js';
 import postsRouter from './routes/posts.js';
@@ -14,6 +16,8 @@ import communityTypeRouter from './routes/communityType.js';
 import userRouter from './routes/users.js';
 import categoryRouter from './routes/categories.js';
 import searchHistoryRouter from './routes/searchHistory.js';
+import deliveryPots from './routes/deliveryPots.js';
+import messagesRouter from './routes/messages.js';
 
 import { notFound, errorHandler } from './middlewares/error.js';
 
@@ -21,6 +25,7 @@ const app = express();
 dotenv.config();
 passportConfig();
 const port = process.env.PORT || 5000;
+const FileStoreInstance = FileStore(session);
 
 app.use(
   cors({
@@ -34,11 +39,12 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
+    store: new FileStoreInstance(),
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-      httpOnly: false,
+      httpOnly: true,
       secure: false,
     },
   })
@@ -54,10 +60,14 @@ app.use('/community-types', communityTypeRouter);
 app.use('/users', userRouter);
 app.use('/categories', categoryRouter);
 app.use('/search-history', searchHistoryRouter);
+app.use('/messages', messagesRouter);
+app.use('/delivery-pots', deliveryPots);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () =>
+const server = app.listen(port, () =>
   console.log('> Server is up and running on port : ' + port)
 );
+
+webSocket(server, app);
