0ca25a33de1e66a66e8465cb1462085cfba536bf
uiyoung <suy2suy@gmail.com>
2023-11-27 12:25:57 +0900

    Refactor: 구글로그인 미들웨어 수정

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -133,33 +133,31 @@ export function kakaoLoginCallback(req, res, next) {
 export function googleLogIn(req, res, next) {
   return passport.authenticate('google', {
     scope: ['email', 'profile'],
-  });
+  })(req, res, next);
 }
 
 export function googleLogInCallback(req, res, next) {
-  try {
-    return (req, res, next) => {
-      passport.authenticate('google', (err, user) => {
+  passport.authenticate('google', (err, user) => {
+    try {
+      if (err) {
+        console.error(err);
+        throw err;
+      }
+
+      if (!user) {
+        throw new Error('error');
+      }
+
+      req.login(user, (err) => {
         if (err) {
           console.error(err);
           return next(err);
         }
-
-        if (!user) {
-          throw new Error('error');
-        }
-
-        return req.login(user, (err) => {
-          if (err) {
-            console.error(err);
-            return next(err);
-          }
-          res.redirect('http://localhost:5173/authorize');
-        });
-      })(req, res, next);
-    };
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
+        res.redirect('http://localhost:5173/');
+      });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  })(req, res, next);
 }
