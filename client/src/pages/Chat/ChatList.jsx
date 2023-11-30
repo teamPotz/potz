e89d9bc4a5ab624d13e36b08b73fb8bab5bb5eca
uiyoung <suy2suy@gmail.com>
2023-11-30 18:36:34 +0900

    Refactor: css 수정

--- client/src/pages/Chat/ChatList.jsx
+++ client/src/pages/Chat/ChatList.jsx
@@ -1,47 +1,15 @@
-import styled from 'styled-components';
-import COLOR from '../../utility/Color';
-import NavBar from '../../components/ui/NavBar';
-import { useAuth } from '../../contexts/AuthContext';
-import { roomSocket } from '../../../socket.js';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import timeAgoFormat from '../../utility/timeAgo.js';
+import COLOR from '../../utility/Color';
 import PotStatus from '../../components/chat/PotStatus.jsx';
-
-const lastMessage = (message) => {
-  let lastMessage;
-
-  switch (message.type) {
-    case 'SYSTEM':
-    case 'REQUEST':
-      lastMessage = message.content.message;
-      break;
-    case 'TEXT':
-      lastMessage = `${message.sender.name}: ${message.content.message}`;
-      break;
-    case 'ORDER':
-      lastMessage = `${message.sender.name}님의 메뉴 선택`;
-      break;
-    case 'ORDER_CONFIRM':
-      lastMessage = `${message.sender.name}님의 메뉴 확인 완료!`;
-      break;
-    case 'DEPOSIT':
-      lastMessage = `${message.sender.name}님의 입금 인증`;
-      break;
-    case 'DEPOSIT_CONFIRM':
-      lastMessage = `${message.sender.name}님의 입금 확인 완료!`;
-      break;
-    default:
-      break;
-  }
-
-  return lastMessage;
-};
+import NavBar from '../../components/ui/NavBar';
+import { roomSocket } from '../../../socket.js';
 
 function ChatList() {
   const [deliveryPots, setDeliveryPots] = useState([]);
   const navigate = useNavigate();
-  const { user } = useAuth();
 
   useEffect(() => {
     async function getDeliveryPots() {
@@ -154,11 +122,10 @@ function ChatList() {
       <Title>배달팟 채팅 목록</Title>
       <div style={styles.sideTitle}>
         <FontMd color={`${COLOR.GRAY_400}`}>
-          지금 참여중인 배달팟 채팅 {deliveryPots.length}
+          지금 참여 중인 배달팟 채팅 {deliveryPots.length}
         </FontMd>
         <FoldButton />
       </div>
-
       <div style={styles.content}>
         {deliveryPots.map((pot) => (
           <Chat
@@ -185,6 +152,7 @@ function ChatList() {
                     <UnreadCounter>{pot._count.messages}</UnreadCounter>
                   )}
                 </div>
+
                 <div style={styles.rowFlex}>
                   <GreenDot />
                   <FontMd color={`${COLOR.GRAY_400}`}>
@@ -195,6 +163,7 @@ function ChatList() {
                     </span>
                   </FontMd>
                 </div>
+
                 <div
                   style={{
                     ...styles.rowFlex,
@@ -248,6 +217,12 @@ const Chat = styled.div`
   box-sizing: border-box;
   gap: 16.33px;
   cursor: pointer;
+
+  &:hover {
+    transform: scale(1.02);
+    transition: 0.2s ease-in-out;
+    backgound-color: ${COLOR.POTZ_PINK_100};
+  }
 `;
 const Content1 = styled.div`
   margin-left: 28px;
@@ -296,6 +271,7 @@ const styles = {
   },
   sideTitle: {
     marginTop: '70px',
+    paddingTop: '4px',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -333,18 +309,50 @@ const styles = {
 };
 
 const FoldButton = () => {
-  <svg
-    width='7'
-    height='6'
-    viewBox='0 0 7 6'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <path
-      d='M3.51469 2.3038L5.86667 4.94845C5.99115 5.08759 6.15954 5.16569 6.33507 5.16569C6.51059 5.16569 6.67899 5.08759 6.80347 4.94845C6.86574 4.879 6.91517 4.79637 6.9489 4.70533C6.98263 4.61429 7 4.51665 7 4.41802C7 4.3194 6.98263 4.22175 6.9489 4.13072C6.91517 4.03968 6.86574 3.95705 6.80347 3.8876L3.98641 0.720007C3.92465 0.649985 3.85117 0.594407 3.7702 0.556479C3.68924 0.518551 3.6024 0.499023 3.51469 0.499023C3.42698 0.499023 3.34014 0.518551 3.25918 0.556479C3.17821 0.594407 3.10473 0.649984 3.04297 0.720007L0.192687 3.8876C0.13111 3.95741 0.082393 4.0402 0.0493291 4.13122C0.0162657 4.22224 -0.000494666 4.3197 1.0777e-05 4.41802C-0.000494675 4.51634 0.0162657 4.61381 0.0493291 4.70483C0.082393 4.79585 0.13111 4.87864 0.192687 4.94845C0.317171 5.08759 0.485564 5.16569 0.66109 5.16569C0.836615 5.16569 1.00501 5.08759 1.12949 4.94845L3.51469 2.3038Z'
-      fill='#808080'
-    />
-  </svg>;
+  return (
+    <svg
+      width='7'
+      height='6'
+      viewBox='0 0 7 6'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M3.51469 2.3038L5.86667 4.94845C5.99115 5.08759 6.15954 5.16569 6.33507 5.16569C6.51059 5.16569 6.67899 5.08759 6.80347 4.94845C6.86574 4.879 6.91517 4.79637 6.9489 4.70533C6.98263 4.61429 7 4.51665 7 4.41802C7 4.3194 6.98263 4.22175 6.9489 4.13072C6.91517 4.03968 6.86574 3.95705 6.80347 3.8876L3.98641 0.720007C3.92465 0.649985 3.85117 0.594407 3.7702 0.556479C3.68924 0.518551 3.6024 0.499023 3.51469 0.499023C3.42698 0.499023 3.34014 0.518551 3.25918 0.556479C3.17821 0.594407 3.10473 0.649984 3.04297 0.720007L0.192687 3.8876C0.13111 3.95741 0.082393 4.0402 0.0493291 4.13122C0.0162657 4.22224 -0.000494666 4.3197 1.0777e-05 4.41802C-0.000494675 4.51634 0.0162657 4.61381 0.0493291 4.70483C0.082393 4.79585 0.13111 4.87864 0.192687 4.94845C0.317171 5.08759 0.485564 5.16569 0.66109 5.16569C0.836615 5.16569 1.00501 5.08759 1.12949 4.94845L3.51469 2.3038Z'
+        fill='#808080'
+      />
+    </svg>
+  );
+};
+
+const lastMessage = (message) => {
+  let lastMessage;
+
+  switch (message.type) {
+    case 'SYSTEM':
+    case 'REQUEST':
+      lastMessage = message.content.message;
+      break;
+    case 'TEXT':
+      lastMessage = `${message.sender.name}: ${message.content.message}`;
+      break;
+    case 'ORDER':
+      lastMessage = `${message.sender.name}님의 메뉴 선택`;
+      break;
+    case 'ORDER_CONFIRM':
+      lastMessage = `${message.sender.name}님의 메뉴 확인 완료!`;
+      break;
+    case 'DEPOSIT':
+      lastMessage = `${message.sender.name}님의 입금 인증`;
+      break;
+    case 'DEPOSIT_CONFIRM':
+      lastMessage = `${message.sender.name}님의 입금 확인 완료!`;
+      break;
+    default:
+      break;
+  }
+
+  return lastMessage;
 };
 
 export default ChatList;
