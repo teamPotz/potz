5ac281570d4da39422f01864f04c2eca0a6e4c60
uiyoung <suy2suy@gmail.com>
2023-12-04 09:28:43 +0900

    Refactor: 로그인한 사용자이름으로 나오도록 수정

--- client/src/pages/ChooseInterest.jsx
+++ client/src/pages/ChooseInterest.jsx
@@ -1,9 +1,5 @@
-import Container from 'react-bootstrap/Container';
-import { Row } from 'react-bootstrap';
-import Col from 'react-bootstrap/Col';
+import { useNavigate } from 'react-router-dom';
 import COLOR from '../utility/Color';
-import Font from '../utility/Font';
-import { useState } from 'react';
 import {
   Burger,
   Cafe,
@@ -14,66 +10,14 @@ import {
   Chicken,
   ChineseFood,
 } from '../components/Category_Food';
-import { useNavigate } from 'react-router-dom';
-
-//contents_container 안에 UI 구현 하시면 됩니다!
+import { useAuth } from '../contexts/AuthContext';
 
 function ChooseInterest() {
+  const { user } = useAuth();
   const navigate = useNavigate();
 
-  let [userName, setUserName] = useState('수현');
-
   const navigateHandler = () => {
-    navigate('/find-community');
-  };
-
-  const backgroundStyle = {
-    backgroundColor: COLOR.POTZ_PINK_200,
-  };
-
-  const style1 = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    paddingTop: '60px',
-    paddingBottom: '28px',
-    height: '100%',
-  };
-  const textBoxStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'start',
-    marginTop: '28px',
-    marginBottom: '28px',
-  };
-  const fontStyle = {
-    fontFamily: Font.FontKor,
-    fontSize: '24px',
-    fontWeight: '700',
-    margin: '0',
-    padding: '0',
-    color: COLOR.GRAY_500,
-    marginBottom: '16x',
-  };
-  const buttonStyle = {
-    backgroundColor: COLOR.POTZ_PINK_200,
-    border: 'none',
-    color: COLOR.POTZ_PINK_DEFAULT,
-    fontSize: '20px',
-    fontWeight: '700',
-    marginTop: '4px',
-    cursor: 'grab',
-  };
-  const categoryContainer = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  };
-  const category = {
-    marginBottom: '28px',
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'space-between',
+    navigate('/community/find');
   };
 
   return (
@@ -81,10 +25,10 @@ function ChooseInterest() {
       <div className='contents_container' style={style1}>
         <div style={textBoxStyle}>
           <div style={fontStyle}>
-            <span>{userName}님이 관심가는</span>
-            <br></br>
+            <span>{user.name}님이 관심가는</span>
+            <br />
             <span>분야를 알려주세요!</span>
-            <br></br>
+            <br />
           </div>
           <div>
             <button onClick={navigateHandler} style={buttonStyle}>
@@ -94,25 +38,73 @@ function ChooseInterest() {
         </div>
         <div style={categoryContainer}>
           <div style={category}>
-            <Burger navigateHandler={navigateHandler}></Burger>
-            <Cafe navigateHandler={navigateHandler}></Cafe>
+            <Burger navigateHandler={navigateHandler} />
+            <Cafe navigateHandler={navigateHandler} />
           </div>
           <div style={category}>
-            <KoreanFood navigateHandler={navigateHandler}> </KoreanFood>
-            <Sushi navigateHandler={navigateHandler}></Sushi>
+            <KoreanFood navigateHandler={navigateHandler} />
+            <Sushi navigateHandler={navigateHandler} />
           </div>
           <div style={category}>
-            <ChineseFood navigateHandler={navigateHandler}></ChineseFood>
-            <Pizza navigateHandler={navigateHandler}></Pizza>
+            <ChineseFood navigateHandler={navigateHandler} />
+            <Pizza navigateHandler={navigateHandler} />
           </div>
           <div style={category}>
-            <Chicken navigateHandler={navigateHandler}></Chicken>
-            <Salad navigateHandler={navigateHandler}></Salad>
+            <Chicken navigateHandler={navigateHandler} />
+            <Salad navigateHandler={navigateHandler} />
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const backgroundStyle = {
+  backgroundColor: COLOR.POTZ_PINK_200,
+};
+
+const style1 = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  paddingTop: '60px',
+  paddingBottom: '28px',
+  height: '100%',
+};
+const textBoxStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'start',
+  marginTop: '28px',
+  marginBottom: '28px',
+};
+const fontStyle = {
+  fontSize: '24px',
+  fontWeight: '700',
+  margin: '0',
+  padding: '0',
+  color: COLOR.GRAY_500,
+  marginBottom: '16x',
+};
+const buttonStyle = {
+  backgroundColor: COLOR.POTZ_PINK_200,
+  border: 'none',
+  color: COLOR.POTZ_PINK_DEFAULT,
+  fontSize: '20px',
+  fontWeight: '700',
+  marginTop: '4px',
+  cursor: 'grab',
+};
+const categoryContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+};
+const category = {
+  marginBottom: '28px',
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
+};
 
 export default ChooseInterest;
