f4053cca32d80468e1da2fb45e6ae3c3505bf206
suhyunlee <leesuhyun05505@gmail.com>
2023-12-04 15:23:26 +0900

    Edit: 스타일 수정

--- client/src/pages/LikedListPage.jsx
+++ client/src/pages/LikedListPage.jsx
@@ -95,7 +95,7 @@ function LikedList() {
     display: 'grid',
     gridTemplateColumns: ' repeat( auto-fit, minmax(160px, 1fr))',
     marginBottom: '90px',
-    overflowX: 'hidden',
+    overflow: 'hidden',
   };
 
   const backgroundStyle = {
