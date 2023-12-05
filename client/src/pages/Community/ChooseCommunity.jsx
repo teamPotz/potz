d9b7c7bbececf72a93e9cc9d9b19631b839021a8
uiyoung <suy2suy@gmail.com>
2023-12-05 16:27:53 +0900

    Fix: 주변 공동체가 없는 경우 수정

--- client/src/pages/Community/ChooseCommunity.jsx
+++ client/src/pages/Community/ChooseCommunity.jsx
@@ -1,32 +1,20 @@
-import COLOR from '../../utility/Color';
-import Font from '../../utility/Font';
-import CommunityComp from '../../components/community/Community';
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router';
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import COLOR from '../../utility/Color';
+import CommunityComp from '../../components/community/Community';
 import MakeCommunity from './MakeCommunity';
-
-const ButtonNew = styled.button`
-  border: none;
-  background-color: ${COLOR.POTZ_PINK_100};
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: ${Font.FontKor};
-  color: ${COLOR.GRAY_400};
-  &:hover {
-    color: ${COLOR.GRAY_500};
-  }
-`;
+import { useAuth } from '../../contexts/AuthContext';
 
 function ChooseCommunity() {
-  let navigate = useNavigate();
-  let location = useLocation();
-  let { latLon } = location.state;
-  let [communityDatas, setCommunityDatas] = useState();
-  let latitude = latLon.lat;
-  let longitude = latLon.lng;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { latLon } = location.state;
+  const [communityDatas, setCommunityDatas] = useState([]);
+  const latitude = latLon.lat;
+  const longitude = latLon.lng;
+  const { user } = useAuth();
 
   useEffect(() => {
     async function fetchCommunityData() {
@@ -35,10 +23,7 @@ function ChooseCommunity() {
           `${
             import.meta.env.VITE_APP_API_URL
           }/communities/search?latitude=${latitude}&longitude=${longitude}`,
-          {
-            method: 'GET',
-            credentials: 'include',
-          }
+          { credentials: 'include' }
         );
         const data = await response.json();
         setCommunityDatas(data);
@@ -49,81 +34,42 @@ function ChooseCommunity() {
     }
 
     fetchCommunityData();
-  }, []);
-
-  const backgroundStyle = {
-    background: COLOR.POTZ_PINK_100,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    height: '100%',
-  };
-
-  const contentsStyle = {
-    marginTop: '100px',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '36px',
-    height: '100vh',
-  };
-
-  const fontStyle = {
-    marginLeft: '30px',
-    fontFamily: Font.FontKor,
-    fontSize: '24px',
-    fontWeight: '700',
-    color: COLOR.GRAY_500,
-  };
-
-  const fontStyle2 = {
-    fontFamily: Font.FontKor,
-    fontSize: '16px',
-    fontWeight: '300',
-    margin: '0',
-    padding: '0',
-    color: COLOR.GRAY_400,
-  };
-
-  const fontStyle3 = {
-    color: COLOR.POTZ_PINK_DEFAULT,
-  };
+  }, [latitude, longitude]);
 
   return (
     <div className='potz_container' style={backgroundStyle}>
       {communityDatas && communityDatas.length < 1 ? (
-        <MakeCommunity></MakeCommunity>
+        <MakeCommunity />
       ) : (
         <div style={contentsStyle}>
           <div style={fontStyle}>
-            <span>수현님 근처에</span>
-            <br></br>
-            {communityDatas ? (
+            <span>{user.name}님 근처에</span>
+            <br />
+            {communityDatas.length > 0 ? (
               <span style={fontStyle3}>{communityDatas.length}</span>
             ) : null}
 
             <span>개의 배달 공동체가 있네요.</span>
-            <br></br>
+            <br />
             <span style={fontStyle2}>
               원하는 공동체에서 배달비를 나누어 보세요.
             </span>
           </div>
+
           <div>
-            {communityDatas
+            {communityDatas.length > 0
               ? communityDatas.map((communityData) => {
                   return (
                     <CommunityComp
                       communityData={communityData}
                       key={communityData.id}
-                    ></CommunityComp>
+                    />
                   );
                 })
               : null}
           </div>
-          <ButtonNew
-            onClick={() => {
-              navigate('/community/types');
-            }}
-          >
+
+          <ButtonNew onClick={() => navigate('/community/types')}>
             직접 공동체 만들기
           </ButtonNew>
         </div>
@@ -131,5 +77,52 @@ function ChooseCommunity() {
     </div>
   );
 }
+
+const ButtonNew = styled.button`
+  border: none;
+  background-color: ${COLOR.POTZ_PINK_100};
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  color: ${COLOR.GRAY_400};
+  &:hover {
+    color: ${COLOR.GRAY_500};
+  }
+`;
+
+const backgroundStyle = {
+  background: COLOR.POTZ_PINK_100,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  height: '100%',
+};
+
+const contentsStyle = {
+  marginTop: '100px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '36px',
+  height: '100vh',
+};
+
+const fontStyle = {
+  marginLeft: '30px',
+  fontSize: '24px',
+  fontWeight: '700',
+  color: COLOR.GRAY_500,
+};
+
+const fontStyle2 = {
+  fontSize: '16px',
+  fontWeight: '300',
+  margin: '0',
+  padding: '0',
+  color: COLOR.GRAY_400,
+};
+
+const fontStyle3 = {
+  color: COLOR.POTZ_PINK_DEFAULT,
+};
 
 export default ChooseCommunity;
