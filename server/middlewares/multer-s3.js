f24d1757f555b0a8bc1bc34c978bb9764b6b3ac0
uiyoung <suy2suy@gmail.com>
2023-12-06 18:35:07 +0900

    Refactor: change acl

--- server/middlewares/multer-s3.js
+++ server/middlewares/multer-s3.js
@@ -34,7 +34,7 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: 'potz-bucket',
-    acl: 'public-read-write',
+    acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key(req, file, cb) {
       const ext = MIME_TYPE_MAP[file.mimetype];
