44c78c5570f07546470e6fd19cbd857188c75323
uiyoung <suy2suy@gmail.com>
2023-12-06 10:52:15 +0900

    fix: update google oauth callback url

--- server/passport/googleStrategy.js
+++ server/passport/googleStrategy.js
@@ -10,7 +10,8 @@ export default function () {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        // callbackURL: '/auth/google/callback',
+        callbackURL: `${process.env.SERVER_URL}/api/auth/kakao/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
