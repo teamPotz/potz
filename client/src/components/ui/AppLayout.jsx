851702b900b3ed693417234319ea65c709c749dc
suhyunlee <leesuhyun05505@gmail.com>
2023-12-07 15:20:52 +0900

    Feat: 사이드 화면 추가

--- client/src/components/ui/AppLayout.jsx
+++ client/src/components/ui/AppLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../../App.css';
+import leftImg from '../../../public/images/graphicImg/potzImg.png';
+import qrImg from '../../../public/images/graphicImg/qrCode.png';
 
 function AppLayout() {
   return (
     <Container className='background'>
       <Row className='row1'>
         <Col className='col1'>
-          <div className='side_container'></div>
+          <div className='side_container'>
+            <img src={qrImg} width={260} height={464}></img>
+          </div>
         </Col>
 
         <Col className='col2'>
@@ -15,7 +19,9 @@ function AppLayout() {
         </Col>
 
         <Col className='col3'>
-          <div className='side_container'></div>
+          <div className='side_container'>
+            <img width={260} height={464} src={leftImg}></img>
+          </div>
         </Col>
       </Row>
     </Container>
